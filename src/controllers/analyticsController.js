398fc07537db61218eefe9c16e3a8133f72eebae
FPt Clothes <hieucute566@gmail.com>
2026-01-08 17:41:34 +0700

    update

--- src/controllers/analyticsController.js
+++ src/controllers/analyticsController.js
@@ -434,7 +434,7 @@ export const getDailyAndProductRevenue = async (req, res) => {
 			matchFilter.createdAtDate.$lt = end;
 		}
 	}
-    // git 44333131232
+    // git 44333131232eqweqw
 	const hasDateRange = startDate && endDate;
 	const hasProduct = product && product !== "undefined" && product !== "null";
 
